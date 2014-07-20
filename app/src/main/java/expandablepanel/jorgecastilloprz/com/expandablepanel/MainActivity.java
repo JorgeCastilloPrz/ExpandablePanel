85ff84a8a9a808718ba6a82663992e986e17671d
Jorge Castillo Perez <jorge.castillo.prz@gmail.com>
2014-07-20 18:10:19 +0200

    ADDED: Booted sample app a little bit

--- app/src/main/java/expandablepanel/jorgecastilloprz/com/expandablepanel/MainActivity.java
+++ app/src/main/java/expandablepanel/jorgecastilloprz/com/expandablepanel/MainActivity.java
@@ -1,17 +1,50 @@
 package expandablepanel.jorgecastilloprz.com.expandablepanel;
 
-import android.support.v7.app.ActionBarActivity;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
+
+import com.jorgecastilloprz.expandablepanel.ExpandablePanelView;
+import com.jorgecastilloprz.expandablepanel.listeners.ExpandableListener;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ExpandableListener {
+
+    private View avatar;
+    private ExpandablePanelView expandablePanelView;
+
+    private Typeface robotoThin;
+    private Typeface robotoLight;
+
+    private TextView userName;
+    private TextView userDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        robotoLight = Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf");
+        robotoThin = Typeface.createFromAsset(getAssets(), "Roboto-Thin.ttf");
+
+        userName = (TextView) findViewById(R.id.userName);
+        userDetails = (TextView) findViewById(R.id.userDetails);
+
+        userName.setTypeface(robotoThin);
+        userDetails.setTypeface(robotoLight);
+
+        avatar = findViewById(R.id.avatar);
+        expandablePanelView = (ExpandablePanelView) findViewById(R.id.expandablePanelView);
+
+        expandablePanelView.attachExpandableListener(this);
     }
 
     @Override
@@ -27,5 +60,33 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onExpandingStarted() {
+    }
+
+    @Override
+    public void onExpandingFinished() {
+        ValueAnimator avatarAnim = ObjectAnimator.ofFloat(avatar, "alpha", 1, 0, 0);
+        avatarAnim.setDuration(1000).setInterpolator(new DecelerateInterpolator());
+
+        avatarAnim.start();
+    }
+
+    @Override
+    public void onShrinkStarted() {
+    }
+
+    @Override
+    public void onShrinkFinished() {
+        ValueAnimator avatarAnim = ObjectAnimator.ofFloat(avatar, "alpha", 0, 1, 1);
+        avatarAnim.setDuration(1000).setInterpolator(new DecelerateInterpolator());
+
+        avatarAnim.start();
+    }
+
+    @Override
+    public void onExpandingTouchEvent(MotionEvent motionEvent) {
     }
 }
---
+++ app/src/main/java/expandablepanel/jorgecastilloprz/com/expandablepanel/components/CircledImageView.java
@@ -0,0 +1,90 @@
+package expandablepanel.jorgecastilloprz.com.expandablepanel.components;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by jorge on 5/15/14.
+ */
+public class CircledImageView extends ImageView {
+
+    public CircledImageView(Context context) {
+        super(context);
+    }
+
+    public CircledImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircledImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Drawable drawable = getDrawable();
+
+        if (drawable == null) {
+            return;
+        }
+
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
+        }
+        Bitmap b =  ((BitmapDrawable)drawable).getBitmap() ;
+        Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
+
+        int w = getWidth(), h = getHeight();
+
+
+        Bitmap roundBitmap =  getCroppedBitmap(bitmap, w);
+        canvas.drawBitmap(roundBitmap, 0,0, null);
+
+    }
+
+    private static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
+
+        Bitmap sbmp;
+
+        if(bmp.getWidth() != radius || bmp.getHeight() != radius)
+            sbmp = Bitmap.createScaledBitmap(bmp, radius, radius, false);
+        else
+            sbmp = bmp;
+
+        Bitmap output = Bitmap.createBitmap(sbmp.getWidth(), sbmp.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xffa19774;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
+
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+//        paint.setDither(true);
+        canvas.drawARGB(0, 0, 0, 0);
+
+        canvas.drawCircle(sbmp.getWidth() / 2+0.7f, sbmp.getHeight() / 2+0.7f,
+                sbmp.getWidth() / 2+0.1f, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(sbmp, rect, rect, paint);
+
+        // draw internal border
+        paint.setColor(Color.parseColor("#ffffff"));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth((float) 30);
+        canvas.drawCircle(sbmp.getWidth()/ 2 + 0.7f, sbmp.getHeight() / 2 + 0.7f, sbmp.getWidth() / 2+0.1f, paint);
+
+        return output;
+    }
+}
